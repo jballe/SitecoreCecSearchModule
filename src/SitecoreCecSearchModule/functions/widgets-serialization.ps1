9e176b76c2af76089b8fb072a2c2cb34f8fa48db
Jesper Balle <jesper@balle-net.dk>
2024-03-13 13:47:29 +0100

    Widgets fix

--- src/SitecoreCecSearchModule/functions/widgets-serialization.ps1
+++ src/SitecoreCecSearchModule/functions/widgets-serialization.ps1
@@ -49,14 +49,15 @@ function Remove-CecWidgetUserDates {
         $Widget.PSObject.Properties.Remove('version')
         $Widget.PSObject.Properties.Remove('widgetId')
         $Widget.PSObject.Properties.Remove('parentWidgetId')
+        $Widget.PSObject.Properties.Remove('live')
 
         foreach ($v in $Widget.variations) {
-            $Widget.PSObject.Properties.Remove('createdAt')
-            $Widget.PSObject.Properties.Remove('updatedAt')
-            $Widget.PSObject.Properties.Remove('status')
-            $Widget.PSObject.Properties.Remove('userId')
-            $Widget.PSObject.Properties.Remove('version')
-            $Widget.PSObject.Properties.Remove('variationId')
+            $v.PSObject.Properties.Remove('createdAt')
+            $v.PSObject.Properties.Remove('updatedAt')
+            $v.PSObject.Properties.Remove('status')
+            $v.PSObject.Properties.Remove('userId')
+            $v.PSObject.Properties.Remove('version')
+            $v.PSObject.Properties.Remove('variationId')
         }
 
         $Widget

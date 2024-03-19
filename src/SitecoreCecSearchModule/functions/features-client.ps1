95302ed49127e3a9c9dec861bb69037e1edad033
Jesper Balle <jesper@balle-net.dk>
2024-03-19 11:48:14 +0100

    Fix non used methods

--- src/SitecoreCecSearchModule/functions/features-client.ps1
+++ src/SitecoreCecSearchModule/functions/features-client.ps1
@@ -1,20 +1,25 @@
 function Get-CecFeatureConfig {
-    [CmdletBinding(DefaultParameterSetName='-Current')]
+    [CmdletBinding(DefaultParameterSetName = '-Current')]
     param(
-        [Parameter(ParameterSetName='-Current')][Switch]$Current,
-        [Parameter(ParameterSetName='-Live')][Switch]$Live,
-        [Parameter(ParameterSetName='-All')][Switch]$All
+        [Parameter(ParameterSetName = '-Current')][Switch]$Current,
+        [Parameter(ParameterSetName = '-Live')][Switch]$Live,
+        [Parameter(ParameterSetName = '-All')][Switch]$All
     )
 
     $result = (Invoke-CecDomainMethod -Path "/microservices/common-editor/configs/current/features").features
-    if($All) {
+    if ($All) {
         return $result
     }
 
-    if($Live) {
+    if ($Live) {
         return $result.live.search
     }
 
+    if ($Current) {
+        return $result.search
+    }
+
+    # Default - or actually unexpected ParameterSetName
     return $result.search
 }
 
@@ -31,8 +36,12 @@ function Set-CecFeatureConfig {
 
         $body = @{
             featureConfigId = $existing.featureConfigId
-            version = $existing.version
-            search = $existing.search
+            version         = $existing.version
+            search          = $existing.search
+        }
+
+        foreach ($name in $Features.PSObject.Property.Name) {
+            $body.$name = $Features.$name
         }
 
         $urlPath = "/microservices/common-editor/configs/current/features"
@@ -44,6 +53,25 @@ function Set-CecFeatureConfig {
             Write-Information ("Would have called PUT on ${urlPath} with body:`n{0}" -f ($Connector | ConvertTo-Json -Depth 30))
         }
 
+        if ($Publish) {
+            Publish-CecFeatureConfig | Out-Null
+        }
+
         $result.features
+    }
+}
+
+function Publish-CecFeatureConfig {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Medium')]
+    param(
+    )
+
+    $urlPath = "/microservices/common-editor/configs/current/versions/draft?"
+
+    if ($Force -or $PSCmdlet.ShouldProcess("SitecoreCeCSearch", 'Send request to service')) {
+        Invoke-CecDomainMethod -Path $urlPath -Method PUT -Body $body
+    }
+    else {
+        Write-Information ("Would have called PUT on ${urlPath} with body:`n{0}" -f ($Connector | ConvertTo-Json -Depth 30))
     }
 }

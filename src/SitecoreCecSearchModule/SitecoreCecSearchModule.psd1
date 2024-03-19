ad816d1d5b20c1f946e35a9dca723818393b18ae
Jesper Balle <jesper@balle-net.dk>
2024-03-19 11:25:28 +0100

    Add Invoke-GetAndWriteCecConfigs

--- src/SitecoreCecSearchModule/SitecoreCecSearchModule.psd1
+++ src/SitecoreCecSearchModule/SitecoreCecSearchModule.psd1
@@ -111,9 +111,10 @@
     'Get-CecCustomerKey',
     'Get-CecDnsEntry',
     'Get-CecEndpoint',
-    'Get-CecApiKeys'
+    'Get-CecApiKeys',
 
-
+    'Invoke-GetAndWriteCecConfigs'
+    
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport      = @()
 
---
+++ src/SitecoreCecSearchModule/functions/aggregated.ps1
@@ -0,0 +1,29 @@
+function Invoke-GetAndWriteCecConfigs {
+    param(
+        $Path,
+        $EnvToken,
+        $Suffix,
+        $Prefix,
+        $TextToken,
+        $ScriptToken,
+        $Domain
+    )
+
+    Get-CecAttribute | Write-CecAttribute -Path $Path -Force
+
+    if("${EnvToken}" -ne "" -and "${Suffix}" -eq "" -and "${Prefix}" -eq "" -and "${TextToken}" -eq "" -and "${ScriptToken}" -eq "") {
+        $Suffix = "_${EnvToken}"
+        $TextToken = "${EnvToken}"
+        $ScriptToken = "_${EnvToken}"
+    }
+
+    $connectorsPath = Join-Path $Path "connectors"
+    Get-CecConnectorsInfo -Suffix $Suffix -Prefix $Prefix | Get-CecConnector `
+    | Remove-CecConnectorPrefix -Suffix:$Suffix -Prefix:$Prefix -TextToken:$TextToken -ScriptToken:$ScriptToken -Domains:$Domains `
+    | Write-CecConnector -Path $connectorsPath -Subfolder
+
+    Get-CecFeatureConfig | Write-CecFeatureConfig -Path $Path
+
+    Get-CecWidgetInfo | Get-CecWidget | Write-CecWidget -Path $Path -Subfolder -Clean
+}
+

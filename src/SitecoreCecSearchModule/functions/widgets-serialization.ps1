7546de3e16e82fff3473357eef845c472a588efb
Jesper Balle <jesper@balle-net.dk>
2024-03-19 11:51:01 +0100

    Support SupportsShouldProcess

--- src/SitecoreCecSearchModule/functions/widgets-serialization.ps1
+++ src/SitecoreCecSearchModule/functions/widgets-serialization.ps1
@@ -17,7 +17,7 @@ function Write-CecWidget {
         }
         $destinationPath = Resolve-Path $destinationPath
 
-        if($Clean) {
+        if ($Clean) {
             Get-ChildItem $destinationPath | Remove-Item -Force -Recurse
         }
     }
@@ -29,35 +29,38 @@ function Write-CecWidget {
             New-Item $folder -ItemType Directory | Out-Null
         }
 
-        $json = $Widget | Remove-CecWidgetUserDate | ConvertTo-Json -Depth 15
+        $json = $Widget | Remove-CecWidgetUserDate -Force | ConvertTo-Json -Depth 15
         $filePath = Join-Path $folder ("{0}.json" -f $Widget.name)
         Set-Content -LiteralPath $filePath -Value $json -Force
     }
 }
 
 function Remove-CecWidgetUserDate {
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
     param(
-        [Parameter(ValueFromPipeline, Mandatory)]$Widget
+        [Parameter(ValueFromPipeline, Mandatory)]$Widget,
+        [Switch]$Force
     )
 
     process {
-        $Widget.PSObject.Properties.Remove('createdAt')
-        $Widget.PSObject.Properties.Remove('updatedAt')
-        $Widget.PSObject.Properties.Remove('status')
-        $Widget.PSObject.Properties.Remove('userId')
-        $Widget.PSObject.Properties.Remove('version')
-        $Widget.PSObject.Properties.Remove('widgetId')
-        $Widget.PSObject.Properties.Remove('parentWidgetId')
-        $Widget.PSObject.Properties.Remove('live')
+        if ($Force -or $PSCmdlet.ShouldProcess("SitecoreCeCSearch", 'Change local model')) {
+            $Widget.PSObject.Properties.Remove('createdAt')
+            $Widget.PSObject.Properties.Remove('updatedAt')
+            $Widget.PSObject.Properties.Remove('status')
+            $Widget.PSObject.Properties.Remove('userId')
+            $Widget.PSObject.Properties.Remove('version')
+            $Widget.PSObject.Properties.Remove('widgetId')
+            $Widget.PSObject.Properties.Remove('parentWidgetId')
+            $Widget.PSObject.Properties.Remove('live')
 
-        foreach ($v in $Widget.variations) {
-            $v.PSObject.Properties.Remove('createdAt')
-            $v.PSObject.Properties.Remove('updatedAt')
-            $v.PSObject.Properties.Remove('status')
-            $v.PSObject.Properties.Remove('userId')
-            $v.PSObject.Properties.Remove('version')
-            $v.PSObject.Properties.Remove('variationId')
+            foreach ($v in $Widget.variations) {
+                $v.PSObject.Properties.Remove('createdAt')
+                $v.PSObject.Properties.Remove('updatedAt')
+                $v.PSObject.Properties.Remove('status')
+                $v.PSObject.Properties.Remove('userId')
+                $v.PSObject.Properties.Remove('version')
+                $v.PSObject.Properties.Remove('variationId')
+            }
         }
 
         $Widget

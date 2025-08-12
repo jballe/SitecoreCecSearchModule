d49945fd83e9ea337fb16ab32d35e1b3b2d47ec1
Jesper Balle <jesper@balle-net.dk>
2025-08-12 20:29:28 +0200

    Split authentication methods to proper files, proper PKCE implementation

--- src/SitecoreCecSearchModule/functions/client.ps1
+++ src/SitecoreCecSearchModule/functions/client.ps1
@@ -16,172 +16,6 @@ function Set-CecClientDefaultProperties {
     Set-Variable -Scope Global -Name ClientDefaultProperties -Value $Value | Out-Null
 }
 
-function Invoke-CecLogin {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', 'Password', Justification = 'Obsolete')]
-    param(
-        [String] $Email,
-        [String] $Password
-    )
-
-    Invoke-CecPasswordAuthentication -Email $Email -Password $Password
-    Invoke-RefreshAccessToken
-}
-
-function Invoke-CecPortalAuthentication {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', 'Password', Justification = 'Obsolete')]
-    param(
-        $Email,
-        [String] $Password,
-        $OrganizationId = "org_Xe6mj0fXtjGPVzFs",
-        $TenantId = "9e276386-a70c-4cc6-e5e7-08dda91b30c7"
-    )
-    $ErrorActionPreference = "STOP"
-    $defaultRequestArguments = (Get-Variable -Scope Global -Name ClientDefaultProperties).Value
-
-    Invoke-WebRequest -SessionVariable "CecLoginSession" -Uri "https://cec.sitecorecloud.io"  -Method GET -UseBasicParsing @defaultRequestArguments | Out-Null
-    $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri "https://account.sitecorecloud.io/login/?redirect=https%3A%2F%2Fcec.sitecorecloud.io&scope=%5B%22portal%22%2C%22search-rec%22%2C%22admin%22%2C%22internal%22%2C%22util%22%2C%22discover%22%2C%22event%22%2C%22ingestion%22%5D"  -Method GET -UseBasicParsing @defaultRequestArguments
-    # Fetch chuncks
-    Write-Information "Requesting account script bundles to find IDP definitions..."
-    $idpDefinition = ([regex]"`"(/_next/[^`"]+.js)`"").Matches($response.Content) | ForEach-Object { 
-        $url = "https://account.sitecorecloud.io$($_.Groups[1].Value)"
-        $content = Invoke-RestMethod -Uri $url @defaultRequestArguments
-        if ($content -match "prod:\s*\{.*?oauth2:\s*(\{sitecoreIdp:\s*\{.*?\}\})") {
-            $Matches[1]
-        }
-    } | ConvertFrom-Json | Select-Object -ExpandProperty sitecoreIdp
-    
-    if($null -eq $idpDefinition) {
-        throw "Could not find IDP definition in the response, please check the login URL or the response content."
-    } else {
-        Write-Information "Found IDP definition for  $($idpDefinition.authorizeUrl)"
-    }
-
-    $verifier = "kPvs5iL6Nl6YhM_-9U2N6lE4fvOquDAtI6FPbt5Dvp4" # This is random bytes converted with base64, removed =, changed + to - and / to _
-    $challenge = "GQ7x9Q-IpB5Kq3ftkozIXyGRjcfsHDBnRA0HfyzKrqs" # This is HMAC hash of verifier
-    $cookie = [System.Net.Cookie]::new('code_verifier', $verifier, "/", ".sitecorecloud.io")
-    $CecLoginSession.Cookies.Add($cookie)
-    $url = $idpDefinition.authorizeUrl + "?response_type=code" +
-    "&client_id=" + $idpDefinition.clientId + 
-    "&redirect_uri=" + [uri]::EscapeDataString($idpDefinition.redirectUrl) + 
-    "&scope=" + [uri]::EscapeDataString($idpDefinition.scope) + 
-    "&audience=" + [uri]::EscapeDataString($idpDefinition.audience) + 
-    "&code_challenge=" + $challenge + 
-    "&code_challenge_method=S256" +
-    "&product_codes=Search%2CDiscover"
-    if ("${organizationId}" -ne "") {
-        $url += "&organization_id=${OrganizationId}"
-    }
-    if ("${tenantId}" -ne "") {
-        $url += "&tenant_id=${TenantId}"
-    }
-
-    $response = Invoke-WebRequest -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction SilentlyContinue -WebSession $CecLoginSession -Uri $url -Method GET -UseBasicParsing @defaultRequestArguments
-    $url = ([uri]$idpDefinition.authorizeUrl).GetLeftPart('Authority') + $response.Headers.Location
-    $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri $url -Method GET -UseBasicParsing @defaultRequestArguments
-
-    do {
-        $formData = New-FormResponseData -Response $response -Values @{
-            username = $Email
-            password = $Password
-        }
-
-        Write-Verbose "Submitting login form to $url"
-        $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri $url -Method POST -UseBasicParsing -Body $formData -ContentType "application/x-www-form-urlencoded" -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction SilentlyContinue @defaultRequestArguments
-        while ($response.StatusCode -eq 302) {
-            $newUrl = $response.Headers.Location | Select-Object -First 1
-            if($newUrl -match "^\/") {
-                $url = ([uri]$url).GetLeftPart('Authority') + $newUrl
-                Write-Verbose "Got redirect to $newUrl will request $url"
-            } else {
-                $url = $newUrl
-            }
-
-            Write-Verbose "Requesting '$url'"
-            $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri $url -Method GET -UseBasicParsing -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction SilentlyContinue @defaultRequestArguments
-        }
-    } while ($response.StatusCode -eq 200 -and $response.Content -match "name=`"username`"")
-
-    $code = $url -split "code=" | Select-Object -Last 1
-    if ("${code}" -eq "") {
-        throw "Could not login, there is no code in url $($url)"
-    }
-
-    $formData = @{
-        grant_type    = "authorization_code"
-        code          = $code
-        code_verifier = $verifier
-        client_id     = $idpDefinition.clientId
-        redirect_uri  = $idpDefinition.redirectUrl
-    }
-    $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri "https://auth.sitecorecloud.io/oauth/token" -Method POST -Body $formData -ContentType "application/x-www-form-urlencoded" -UseBasicParsing @defaultRequestArguments
-    $token = $response.Content | ConvertFrom-Json
-    Set-CecRefreshToken -RefreshToken $token.refresh_token
-    Set-CecAccessToken -AccessToken $token.access_token
-}
-
-function New-FormResponseData {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Scope = 'Function')]
-    param(
-        $Response,
-        $Values = @{}
-    )
-
-
-    $formData = @{}
-    $inputFields = ([regex]"<input[^>]+>").Matches($Response.Content)
-    foreach ($f in $inputFields) {
-        $name = ([regex]"name\s*=\s*""([^""]+)""").Match($f.Value).Groups[1].Value
-        $value = ([regex]"value\s*=\s*""([^""]+)""").Match($f.Value).Groups[1].Value
-
-        if ($Values.ContainsKey($name)) {
-            $value = $Values[$name]
-        }
-
-        if ($name -and $value) {
-            $formData[$name] = $value
-        }
-    }
-
-    $formData
-}
-
-function Invoke-CecPasswordAuthentication {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', 'Password', Justification = 'Obsolete')]
-    param(
-        $Email,
-        [String] $Password
-    )
-    $ErrorActionPreference = "STOP"
-    $defaultRequestArguments = (Get-Variable -Scope Global -Name ClientDefaultProperties).Value
-
-    $url = "https://discover.sitecorecloud.io/account/1/authenticate/password/cec"
-    $body = @{
-        email    = $Email
-        password = $Password
-        scope    = @("portal", "search-rec", "admin", "internal", "util", "discover", "event", "ingestion")
-        target   = "https://cec.sitecorecloud.io"
-    } | ConvertTo-Json
-
-    $response = Invoke-RestMethod -Uri $url -ContentType application/json -Method POST -Body $body @defaultRequestArguments
-    if ($response.PSObject.Properties.Name -contains "error-id") {
-        throw  ("Error from login: {0} {1} ({2})" -f $response.type, $response.message, $response.code)
-    }
-    if (-not $response.PSObject.Properties.Name -contains "redirectUrl") {
-        throw ("Unexpected login response:`n{0}" -f ($response | ConvertTo-Json -Depth 15))
-    }
-
-    $redirectUrl = $response.redirectUrl
-    $result = $redirectUrl -match "https://cec.sitecorecloud.io#refresh_token=([^&]+)$"
-    if ($result -eq $false -or $Matches.Count -lt 2) {
-        Write-Error $response
-        throw "Could not login, did not find refresh_token"
-    }
-
-    $refreshToken = $Matches[1]
-    Set-CecRefreshToken -RefreshToken $refreshToken
-    Write-Information "Successfully authenticated"
-}
-
 function Set-CecRefreshToken {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Scope = 'Function')]
     param(
@@ -244,12 +78,14 @@ function Invoke-CecGlobalMethod {
         Headers     = @{ Authorization = "Bearer ${token}" }
         ContentType = "application/json"
     }
+    Write-Verbose "Invoking global method ${Path}"
     Invoke-RestMethod @params @defaultRequestArguments -Body:$Body
 }
 
 function Set-CecDomainContext {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Scope = 'Function')]
     param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
         $Id
     )
 
@@ -262,7 +98,7 @@ function Get-CecDomainContext {
 
 function Invoke-CecDomainMethod {
     param(
-        [string]$Path = "/micorservices/common-editor/connectors",
+        [string]$Path = "/microservices/common-editor/connectors",
         [Microsoft.PowerShell.Commands.WebRequestMethod]$Method = "Get",
         [object]$Body,
         [string]$DomainScope = "portal",
@@ -297,6 +133,7 @@ function Invoke-CecDomainMethod {
     }
 
     try {
+        Write-Verbose "Invoking CEC Domain method ${Path} at ${url} with ${Method} method"
         $response = Invoke-RestMethod @params @defaultRequestArguments -ErrorAction SilentlyContinue
         return $response
     }
---
+++ src/SitecoreCecSearchModule/functions/client_auth_portal.ps1
@@ -0,0 +1,146 @@
+function Invoke-CecPortalAuthentication {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingPlainTextForPassword', 'Password', Justification = 'Obsolete')]
+    param(
+        $Email,
+        [String] $Password,
+        $OrganizationId = "org_Xe6mj0fXtjGPVzFs",
+        $TenantId = "9e276386-a70c-4cc6-e5e7-08dda91b30c7"
+    )
+    $ErrorActionPreference = "STOP"
+    $defaultRequestArguments = (Get-Variable -Scope Global -Name ClientDefaultProperties).Value
+
+    Invoke-WebRequest -SessionVariable "CecLoginSession" -Uri "https://cec.sitecorecloud.io"  -Method GET -UseBasicParsing @defaultRequestArguments | Out-Null
+    $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri "https://account.sitecorecloud.io/login/?redirect=https%3A%2F%2Fcec.sitecorecloud.io&scope=%5B%22portal%22%2C%22search-rec%22%2C%22admin%22%2C%22internal%22%2C%22util%22%2C%22discover%22%2C%22event%22%2C%22ingestion%22%5D"  -Method GET -UseBasicParsing @defaultRequestArguments
+    # Fetch chuncks
+    Write-Information "Requesting account script bundles to find IDP definitions..."
+    $idpDefinition = ([regex]"`"(/_next/[^`"]+.js)`"").Matches($response.Content) | ForEach-Object { 
+        $url = "https://account.sitecorecloud.io$($_.Groups[1].Value)"
+        $content = Invoke-RestMethod -Uri $url @defaultRequestArguments
+        if ($content -match "prod:\s*\{.*?oauth2:\s*(\{sitecoreIdp:\s*\{.*?\}\})") {
+            $Matches[1]
+        }
+    } | ConvertFrom-Json | Select-Object -ExpandProperty sitecoreIdp
+    
+    if($null -eq $idpDefinition) {
+        throw "Could not find IDP definition in the response, please check the login URL or the response content."
+    } else {
+        Write-Information "Found IDP definition for  $($idpDefinition.authorizeUrl)"
+    }
+
+    # Using OIDC PKCE (Proof Key for Code Exchange) to enhance security
+    $pkce = CreatePkceValues
+    $cookie = [System.Net.Cookie]::new('code_verifier', $pkce.Verifier, "/", ".sitecorecloud.io")
+    $CecLoginSession.Cookies.Add($cookie)
+    $url = $idpDefinition.authorizeUrl + "?response_type=code" +
+    "&client_id=" + $idpDefinition.clientId + 
+    "&redirect_uri=" + [uri]::EscapeDataString($idpDefinition.redirectUrl) + 
+    "&scope=" + [uri]::EscapeDataString($idpDefinition.scope) + 
+    "&audience=" + [uri]::EscapeDataString($idpDefinition.audience) + 
+    "&code_challenge=" + $pkce.Challenge + 
+    "&code_challenge_method=S256" +
+    "&product_codes=Search%2CDiscover"
+    if ("${organizationId}" -ne "") {
+        $url += "&organization_id=${OrganizationId}"
+    }
+    if ("${tenantId}" -ne "") {
+        $url += "&tenant_id=${TenantId}"
+    }
+
+    $response = Invoke-WebRequest -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction SilentlyContinue -WebSession $CecLoginSession -Uri $url -Method GET -UseBasicParsing @defaultRequestArguments
+    $url = ([uri]$idpDefinition.authorizeUrl).GetLeftPart('Authority') + $response.Headers.Location
+    $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri $url -Method GET -UseBasicParsing @defaultRequestArguments
+
+    do {
+        $formData = New-FormResponseData -Response $response -Values @{
+            username = $Email
+            password = $Password
+        }
+
+        Write-Verbose "Submitting login form to $url"
+        $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri $url -Method POST -UseBasicParsing -Body $formData -ContentType "application/x-www-form-urlencoded" -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction SilentlyContinue @defaultRequestArguments
+        while ($response.StatusCode -eq 302) {
+            $newUrl = $response.Headers.Location | Select-Object -First 1
+            if($newUrl -match "^\/") {
+                $url = ([uri]$url).GetLeftPart('Authority') + $newUrl
+                Write-Verbose "Got redirect to $newUrl will request $url"
+            } else {
+                $url = $newUrl
+            }
+
+            Write-Verbose "Requesting '$url'"
+            $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri $url -Method GET -UseBasicParsing -MaximumRedirection 0 -SkipHttpErrorCheck -ErrorAction SilentlyContinue @defaultRequestArguments
+        }
+    } while ($response.StatusCode -eq 200 -and $response.Content -match "name=`"username`"")
+
+    $code = $url -split "code=" | Select-Object -Last 1
+    if ("${code}" -eq "") {
+        throw "Could not login, there is no code in url $($url)"
+    }
+
+    $formData = @{
+        grant_type    = "authorization_code"
+        code          = $code
+        code_verifier = $pkce.Verifier
+        client_id     = $idpDefinition.clientId
+        redirect_uri  = $idpDefinition.redirectUrl
+    }
+    $response = Invoke-WebRequest -WebSession $CecLoginSession -Uri "https://auth.sitecorecloud.io/oauth/token" -Method POST -Body $formData -ContentType "application/x-www-form-urlencoded" -UseBasicParsing @defaultRequestArguments
+    $token = $response.Content | ConvertFrom-Json
+    Set-CecRefreshToken -RefreshToken $token.refresh_token
+    Set-CecAccessToken -AccessToken $token.access_token
+}
+
+function CreatePkceValues {
+    # Using OIDC PKCE (Proof Key for Code Exchange) to enhance security
+    # https://github.com/darrenjrobinson/PKCE/blob/main/PKCE.psm1
+    param(
+        # Length must be between 43 and 128 characters
+        [int]$Length = 43
+    )
+
+    # Code verifier is just random string characters, in CEC it is using Crypto module random bytes and therefore base64 and then removing the padding and replacing characters
+    # we will just generate a string of valid characters
+    $codeVerifier = -join (((48..57) * 4) + ((65..90) * 4) + ((97..122) * 4) | Get-Random -Count $Length | ForEach-Object { [char]$_ })
+
+    # Hash the verifier
+    $hashAlgo = [System.Security.Cryptography.HashAlgorithm]::Create('sha256')
+    $hash = $hashAlgo.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($codeVerifier))
+    $b64Hash = [System.Convert]::ToBase64String($hash)
+    $code_challenge = $b64Hash.Substring(0, 43)
+    
+    # Encode by replacing characters
+    $code_challenge = $code_challenge.Replace("/","_")
+    $code_challenge = $code_challenge.Replace("+","-")
+    $code_challenge = $code_challenge.Replace("=","")
+
+    return @{
+        Verifier  = $codeVerifier
+        Challenge = $code_challenge
+    }
+}
+
+function New-FormResponseData {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '', Scope = 'Function')]
+    param(
+        $Response,
+        $Values = @{}
+    )
+
+
+    $formData = @{}
+    $inputFields = ([regex]"<input[^>]+>").Matches($Response.Content)
+    foreach ($f in $inputFields) {
+        $name = ([regex]"name\s*=\s*""([^""]+)""").Match($f.Value).Groups[1].Value
+        $value = ([regex]"value\s*=\s*""([^""]+)""").Match($f.Value).Groups[1].Value
+
+        if ($Values.ContainsKey($name)) {
+            $value = $Values[$name]
+        }
+
+        if ($name -and $value) {
+            $formData[$name] = $value
+        }
+    }
+
+    $formData
+}
